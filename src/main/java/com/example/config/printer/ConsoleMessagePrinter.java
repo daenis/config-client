bec785800b04e4d53d7ce7a8aa60c16ee5598561
daenis <dkalaygia@gmail.com>
2021-06-20 19:45:37 -1000

    Fixed spacing in message

--- src/main/java/com/example/config/printer/ConsoleMessagePrinter.java
+++ src/main/java/com/example/config/printer/ConsoleMessagePrinter.java
@@ -11,7 +11,7 @@ public class ConsoleMessagePrinter implements MessagePrinter {
 
     @Override
     public void printMessage(String message) {
-        System.out.println(name + "- New Message:");
+        System.out.println(name + " - New Message:");
         System.out.println(message);
     }
 }
